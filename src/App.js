570b828c80a8d4b46dc3279cd95ed03e79939d78
Sajal Kumar <122788559+SajalKumar2002@users.noreply.github.com>
2025-04-15 18:14:17 +0530

    updatingFolder

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { CountProvider } from './context/createContext';
 
-import UseStateCounter from './screens/UseStateCounter';
-import UseReducerCounter from './screens/UseReducerCounter';
-import UseRedux from './screens/UseRedux'
-import UseRedux2 from './screens/UseRedux2'
-import UseRef from './screens/UseRef';
-import UseContext from './screens/UseContext';
-import MemoApi from './screens/MemoApi';
-import UseCallBack from './screens/UseCallBack';
-import UseMemo from './screens/useMemo';
+import UseStateCounter from './hooks/UseStateCounter';
+import UseReducerCounter from './hooks/UseReducerCounter';
+import UseRedux from './hooks/UseRedux'
+import UseRedux2 from './hooks/UseRedux2'
+import UseRef from './hooks/UseRef';
+import UseContext from './hooks/UseContext';
+import MemoApi from './hooks/MemoApi';
+import UseCallBack from './hooks/UseCallBack';
+import UseMemo from './hooks/useMemo';
 
 export default function App() {
   return (
